5cd7ab3aeb12bcb9fafb4ce0792c2977739cc98d
Chris Hunley <cshunley21@gmail.com>
2021-11-03 09:30:53 -0500

    Fixed errors

--- app.js
+++ app.js
@@ -1,52 +1,114 @@
-// 1
+// ASYNC BASICS EXERCISE
 
-function mainCourse(m, callback) {
+// 1
+let name1 = "Luke Skywalker";
+
+function lukeName() {
+  console.log(`Question 1`);
+  console.log(name1);
+}
+
+// function luke(n) {
+//   setTimeout(() => {
+//     name1 = n;
+//   }, 1000);
+// }
+// luke(`Mark Hamill`);
+// lukeName();
+
+function luke(n, callback) {
+  setTimeout(() => {
+    name1 = n;
+    callback();
+  }, 1000);
+}
+luke(`Mark Hamill`, lukeName);
+
+// function lukeName(n, callback){
+//   setTimeout(() => {
+//     callback(n);
+//   }, 1000);
+// }
+// lukeName(`Mark Hamill`, (name1) => {
+//   console.log(name1)
+// });
+
+// 2
+let name2 = "Han Solo";
+
+function hanName() {
+  console.log(`Question 2`);
+  console.log(name2);
+}
+
+// function han(n) {
+//   setTimeout(() => {
+//     name2 = n;
+//   }, 2000);
+// }
+// han(`Harrison Ford`);
+// hanName();
+
+function han(n) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      callback(m);
-    }, 1000);
-  }
-  mainCourse(`Turkey`, (main1) => console.log(main1));
-  
-  //   2
-  function side(s) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(s);
-      }, 2000);
-    });
-  }
-  side(`Stuffing`).then((s1) => console.log(s1));
-  
-  // 3
-  function dessert(d) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(d);
-      }, 4000);
-    });
-  }
-  async function dessertFunc() {
-    const d1 = await dessert("Pumpkin Pie");
-    console.log(d1);
-  }
-  dessertFunc();
-  
-  // 4
-  const p1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("First Promise");
+      name2 = n;
+      resolve();
     }, 2000);
   });
-  const p2 = new Promise((resolve, reject) => {
+}
+han(`Harrison Ford`)
+  .then(hanName);
+
+// 3
+let name3 = "Leia Organa";
+
+function leiaName() {
+  console.log(`Question 3`);
+  console.log(name3);
+}
+
+// function leia(n) {
+//   setTimeout(() => {
+//     name3 = n;
+//   }, 3000);
+// }
+// leia('Carrie Fisher');
+// leiaName();
+
+function leia(n) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("Second Promise");
+      name3 = n;
+      resolve();
     }, 3000);
   });
-  const p3 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Third Promise");
-    }, 4000);
-  });
-  
-  Promise.all([p1, p2, p3])
-  .then(p => console.log(p));
+}
+
+async function princess(){
+  await leia(`Carrie Fisher`);
+  leiaName();
+}
+princess();
+
+// BONUS
+
+// 4
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('First Promise')
+  }, 2000);
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Second Promise')
+  }, 3000);
+});
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Third Promise')
+  }, 4000);
+});
+
+Promise.all([p1, p2, p3])
+.then((tur) => console.log(tur))
